2a53f9ed9a98b4caf227a51e5799b0748e4239d6
Rajkumar <thecodeheist01@gmail.com>
2026-02-12 23:22:54 +0530

    db and fe setup

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,8 @@
-
+import {prisma} from "@repo/db"
 export default function Page() {
   return (
    <div>
+     
     <h1 className="bg-red-400"> Hello Raj</h1>
    </div>
   );
---
+++ packages/db/prisma/migrations/20260212174157_first/migration.sql
@@ -0,0 +1,24 @@
+-- CreateEnum
+CREATE TYPE "USerRole" AS ENUM ('admin', 'applicant', 'employer');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "userName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "role" "USerRole" NOT NULL DEFAULT 'applicant',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_userName_key" ON "User"("userName");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
