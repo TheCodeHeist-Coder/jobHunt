cf0869558411bdde5f8fe7b89677e3321bd94c21
Rajkumar <thecodeheist01@gmail.com>
2026-02-14 00:01:17 +0530

    routing_done

---
+++ apps/backend/src/controllers/auth.controller.ts
@@ -0,0 +1,13 @@
+ import {prisma} from "@repo/db"
+import type { RequestHandler } from "express"
+
+export const registerController:RequestHandler = async(req, res) => {
+
+
+
+}
+
+
+export const loginController:RequestHandler  = async(req , res) => {
+    
+}
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,1 +1,18 @@
+import express from 'express'
+import {configDotenv} from 'dotenv';
+configDotenv()
+ import authRoutes from "./routes/auth.route.js"
 
+const app = express();
+app.use(express.json())
+
+const PORT = process.env.PORT as string;
+
+
+app.use("/api/v1/user/", authRoutes)
+
+
+
+app.listen(PORT , () => {
+    console.info(`hey! I'm Server`)
+})
